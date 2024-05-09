09b043f410ac92ffdc79e180984fb63942173538
fantastik8971 <161023430+fantastik8971@users.noreply.github.com>
2024-05-09 22:37:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+    
+let userName1 = prompt(" Ismingizni kiriting");
+let userSurname = prompt("Familyangizni kiriting");
+let userage = prompt("Yoshingizni kiriting");
+let userfan = prompt("Yoqtirgan faningizni kiriting");
+let usertuman = prompt("Yashaydigan tumaningizni kiriting");
+let =alert("Javobingiz uchun rahmat");
+
+console.log(`Name:${userName1}
+Familya:${userSurname}
+Age:${userage}
+Fan:${userfan}
+Tuman:${usertuman}`);
